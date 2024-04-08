6a700f85cf92e752f7c626e389a4b352437eef2f
art_panarin <artemiypanarin@yandex.ru>
2024-04-08 14:15:23 +0300

    u

--- Task3/main.cpp
+++ Task3/main.cpp
@@ -1,12 +1,54 @@
-#include "mainwindow.h"
+//#include "mainwindow.h"
 
 #include <QApplication>
 #include <QStandardItemModel>
 #include <QColumnView>
+#include <QFile>
+#include <QXmlStreamReader>
+#include <QCoreApplication>
+#include <QDir>
+#include <QDebug>
+
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+
+    QString path = "Task3/schedule.xml";
+    QString QDir::current().absoluteFilePath(path);
+
+    QFile file(path);
+    if (!file.open(QIODevice::ReadOnly | QIODevice::Text))
+    {
+        qDebug() << "Failed to open file";
+        return 1;
+    }
+
+    QXmlStreamReader xmlReader(&file);
+
+
+        while (!xmlReader.atEnd() && !xmlReader.hasError())
+        {
+        QXmlStreamReader::TokenType token = xmlReader.readNext();
+        if (token == QXmlStreamReader::StartElement)
+        {
+            qDebug() << "Element: " << xmlReader.name().toString();
+            if (xmlReader.isStartElement())
+            {
+                QXmlStreamAttributes attributes = xmlReader.attributes();
+                foreach (QXmlStreamAttribute attribute, attributes)
+                {
+                    qDebug() << "  Attribute: " << attribute.name().toString() << " = " << attribute.value().toString();
+                }
+            }
+        }
+        else if (token == QXmlStreamReader::Characters && !xmlReader.isWhitespace())
+        {
+            qDebug() << "  Value: " << xmlReader.text().toString();
+        }
+    }
+
+
 
     QStandardItemModel model;
 
@@ -25,7 +67,7 @@ int main(int argc, char *argv[])
     columnView.show();
 
 
-    MainWindow w;
-    w.show();
+    //MainWindow w;
+    //w.show();
     return a.exec();
 }
